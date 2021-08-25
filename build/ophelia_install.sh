108b8853a13bad804d1c38df137e00fb154a66c9
LuisFalva <luis.vargasfavero@gmail.com>
2021-08-25 02:52:56 -0500

    [UPDATE] multiple heritage from DataFrame class in ophelia.functions

--- build/ophelia_install.sh
+++ build/ophelia_install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo '========================================================'
-echo "[Ophelia] Installing Ophelia-0.1.dev0 package locally..."
-echo '========================================================'
+echo '====================================================='
+echo "[Ophelia] Installing Ophelia-0.1.dev0 package locally"
+echo '====================================================='
 
 make install
 
-echo '********************************************************'
-echo 'Ophelia is now installed in your local env. Have fun! =)'
-echo '********************************************************'
+echo '******************************************************************'
+echo '[Ophelia] Ophelia is now installed in your local env. Have fun! =)'
+echo '******************************************************************'
---
+++ build/ophelia_update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo '========================================================='
+echo "[Ophelia] Updating Ophelia-0.1.dev0 package in local env."
+echo '========================================================='
+
+# Lets fetch all new generated metadata from latest push
+git fetch --all
+
+# Get back the 2 input arguments, 1 remote: {origin, upstream}, 2 branch: {develop, main}
+GIT_REMOTE=${1}
+GIT_BRANCH=${2}
+
+# Now lets test if the above arguments are given to the env
+test -z ${GIT_REMOTE} && echo "Git remote must be specified." && exit 1
+test -z ${GIT_BRANCH} && echo "Git branch must be specified." && exit 1
+
+# Pull all the latest changes from Ophelia git repo
+git pull "${GIT_REMOTE}" "${GIT_BRANCH}"
+
+# Now lets update ophelia package
+./build/ophelia_install.sh
+
+echo '********************************************'
+echo '[Ophelia] Successfully built Ophelia \o/ !!!'
+echo '********************************************'
