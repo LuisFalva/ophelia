edc8e52ba4803d2e8d6b679ab91aff949169fa9e
LuisFalva <luis.vargasfavero@gmail.com>
2021-08-29 13:29:16 -0500

    [ADD] improve SparkSession class

--- ophelia/session/spark.py
+++ ophelia/session/spark.py
@@ -15,6 +15,22 @@ class OpheliaSpark:
     def __build_spark(self):
         self.ophelia_spark = SparkSession.builder.appName("No 'appName' configured").getOrCreate()
 
+    def __app_name(self):
+        OpheliaSpark.__logger.info("Spark App Name")
+        return self.ophelia_spark.sparkContext.appName
+
+    def __spark_version(self):
+        OpheliaSpark.__logger.info("Spark Version")
+        return self.ophelia_spark.version
+
+    def __spark_ui_port(self):
+        """
+        Print spark UI port server for tracking Spark activity
+        :return: spark ui port address
+        """
+        OpheliaSpark.__logger.info("Spark UI Port Address")
+        return self.ophelia_spark._sc.uiWebUrl
+
     def build_spark_session(self, app_name: str = None) -> SparkSession:
         """
         Build spark session is the builder method to initialize spark session under the hood of Ophelia
@@ -24,14 +40,16 @@ class OpheliaSpark:
         if not app_name:
             OpheliaSpark.__logger.info("Build Spark Session")
             self.__build_spark()
-            OpheliaSpark.__logger.info("Spark Version: " + self.ophelia_spark.version)
+            OpheliaSpark.__logger.info("Spark Version: " + self.__spark_version())
             OpheliaSpark.__logger.warning("Please, Be Aware To Set App Name Next Time...")
+            OpheliaSpark.__logger.info("Spark UI Address: '" + self.__spark_ui_port())
             return self.ophelia_spark
         else:
             OpheliaSpark.__logger.warning("Initializing Spark Session")
             self.__build_spark_app(app_name=app_name)
-            OpheliaSpark.__logger.info("Spark Version: " + self.ophelia_spark.version)
-            OpheliaSpark.__logger.info("This Is: '" + self.ophelia_spark.sparkContext.appName + "' App")
+            OpheliaSpark.__logger.info("Spark Version: " + self.__spark_version())
+            OpheliaSpark.__logger.info("This Is: '" + self.__app_name() + "' App")
+            OpheliaSpark.__logger.info("Spark UI Address: '" + self.__spark_ui_port())
             return self.ophelia_spark
 
     def build_spark_context(self):
